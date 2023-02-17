1ff84f0189402362ae08229fcc977c70c1f1d993
Daniil Bavykin <daniil.bavykin.dev@gmail.com>
2023-02-17 15:52:00 +0200

    implement the User Authentication project

---
+++ src/store/auth-context.js
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect } from "react";
+
+const AuthContext = createContext({
+  isLoggedIn: false,
+  onLogin: (email, password) => {},
+  onLogout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", "0");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
